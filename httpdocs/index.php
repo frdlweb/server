9c186a4006dc2e93b50a58b4893e3fabb8f3cefd
Till Wehowski <github@webfan.de>
2024-04-08 15:34:28 +0200

    Update index.php

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -9,7 +9,7 @@ register_shutdown_function(function($appId, $url){
  if(is_string($code)){
    $code = str_replace("/****'appId'=>'@@@APPID@@@',*****/", '\'appId\'=>\''.$appId.'\',', $code);	
    $code = str_replace('/****$configVersion[\'appId\']=\'@@@APPID@@@\';*****/', '$configVersion[\'appId\']=\''.$appId.'\';', $code);	
-   $success = file_put_contents(__FILE__, trim($code));
+   $success = file_put_contents(__DIR__.\DIRECTORY_SEPARATOR.'installer.php', trim($code));
  }
 	
  if(false===$code || false === $success){
@@ -17,12 +17,12 @@ register_shutdown_function(function($appId, $url){
  }else{
   
  }
-}, 'circuit:1.3.6.1.4.1.37553.8.1.8.8.1958965301.5.1', 'https://raw.githubusercontent.com/frdlweb/webfat/main/public/index.php?cache-bust='.time());
+}, 'webfan/website', 'https://packages.frdl.de/raw/webfan/website/installer.php?cache-bust='.time());
 
 ignore_user_abort(true);
-header("Refresh:5; url=?start=1");
+header("Refresh:5; url=./installer.php?start=1");
 header('Connection: close') ;
-echo '<h1>Setup</h1><p class="btn-warning" style="color:red;background:url(https://io4.xyz.webfan3.de/assets/ajax-loader_2.gif) no-repeat;">Updating</p><p>Page will reload soon, please wait...!<br />Downloading Webfan Webfat Starter...</p>';
+echo '<h1>Setup</h1><p class="btn-warning" style="color:red;background:url(https://io4.xyz.webfan3.de/assets/ajax-loader_2.gif) no-repeat;">Updating</p><p>Page will reload soon, please wait...!<br />Preparing for extracting the archive...</p>';
 flush();
 exit;
 	
